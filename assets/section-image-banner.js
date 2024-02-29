b7b239bb66fe6c461823c61f3be784f3b905934d
VijayMarmetian <vijay.k@marmeto.com>
2024-02-29 16:21:42 +0530

    design of the section is modified

--- assets/section-image-banner.js
+++ assets/section-image-banner.js
@@ -1,16 +1,25 @@
+
 class ImageBanner extends HTMLElement{
-    constructor(){
-      super();
-      this.sectionId = this.dataset.sectionId
-      this.querySelector("#loadContentButton").addEventListener("click", this.renderSection.bind(this))
-    }
-    renderSection(){
-      fetch(`window.location.pathname?section_id=${this.sectionId}`)
-      .then(res=>res.text())
-      .then(html=>{
-        const htmlResponse = new DOMParser().parseFromString(html, 'text/html')
-        this.insertAdjacentHTML('beforeend', htmlResponse.querySelector("image-banner").innerHTML);
-      })
-    }
+  constructor(){
+    super();
+    this.sectionId = this.dataset.sectionId
+    document.querySelector("#loadContentButton").addEventListener("click", this.renderSection.bind(this))
+    document.querySelector("#scrollLeft").addEventListener("click", this.moveToLeft.bind(this))
   }
-  customElements.define("image-banner", ImageBanner)
+  renderSection(){
+    fetch(`window.location.pathname?section_id=${this.sectionId}`)
+    .then(res=>res.text())
+    .then(html=>{
+      const htmlResponse = new DOMParser().parseFromString(html, 'text/html')
+      this.insertAdjacentHTML('beforeend', htmlResponse.querySelector("image-banner").innerHTML);
+      this.moveToEnd()
+    })
+  }
+  moveToLeft() {
+    this.scrollLeft -= 300 
+  }
+  moveToEnd() {
+    this.scrollLeft = this.scrollWidth;
+  }
+}
+customElements.define("image-banner", ImageBanner)
